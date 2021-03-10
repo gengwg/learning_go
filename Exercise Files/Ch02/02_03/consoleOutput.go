101b88b4f0db1ba7019b96972421776e0f91b296
Weigang Geng <gengwg@fb.com>
2021-03-10 11:28:49 -0800

    console outputs

--- Exercise Files/Ch02/02_03/consoleOutput.go
+++ Exercise Files/Ch02/02_03/consoleOutput.go
@@ -3,11 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	str1 := "The quick red fox"
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
-	//aNumber := 42
-	//isTrue := isTrue
-		
+	aNumber := 42
+	isTrue := true
+
+	// if have a value that declared, you must address it in the code
+	stringLength, _ := fmt.Println(str1, str2, str3)
+
+	//if err == nil {
+		// Println function will add space between
+		fmt.Println("String length:", stringLength)
+	//}
+
+	// print verbals
+	fmt.Printf("Value of aNumber: %v\n", aNumber)
+	fmt.Printf("Value of isTrue: %v\n", isTrue)
+	fmt.Printf("Value of aNumber as float: %.2f\n", float64(aNumber))
+
+	// print types
+	fmt.Printf("Data types: %T, %T, %T, %T, and %T\n",
+		str1, str2, str3, aNumber, isTrue)
+
+
+	// assign printf output to a string
+	myString :=	fmt.Sprintf("Data types as var: %T, %T, %T, %T, and %T\n",
+		str1, str2, str3, aNumber, isTrue)
+	fmt.Print(myString)
 }
