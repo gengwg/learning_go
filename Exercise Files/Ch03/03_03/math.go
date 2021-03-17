fb12ecbc02a810d5ef5f862532438368ca49276b
Weigang Geng <gengwg@fb.com>
2021-03-17 10:49:27 -0700

    formatting flolat using big.Float

--- Exercise Files/Ch03/03_03/math.go
+++ Exercise Files/Ch03/03_03/math.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 )
 
 func main() {
@@ -13,4 +14,12 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	floatSum := f1 + f2 + f3
 	fmt.Println("Float sum: ", floatSum)
+
+	var b1, b2, b3, bigSum big.Float
+	b1.SetFloat64(23.5)
+	b2.SetFloat64(65.1)
+	b3.SetFloat64(76.3)
+
+	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
+	fmt.Printf("BigSum = %.10g\n", &bigSum)
 }
