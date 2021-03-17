65a582755ff15cc6904bd46af0a5c7c16214f33d
Weigang Geng <gengwg@fb.com>
2021-03-17 10:38:33 -0700

    console input

--- Exercise Files/Ch02/02_04/consoleInput.go
+++ Exercise Files/Ch02/02_04/consoleInput.go
@@ -2,8 +2,32 @@ package main
 
 import (
     "fmt"
+    "bufio"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-    
+
+    // var s string
+    // fmt.Scanln(&s)
+    // fmt.Println(s)
+
+    // enter string
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter text: ")
+    str, _ := reader.ReadString('\n')
+    fmt.Println(str)
+
+    // enter number
+    fmt.Print("Enter a number: ")
+    str, _ = reader.ReadString('\n')
+    // need trim string \n before and after
+    f, err := strconv.ParseFloat(strings.TrimSpace(str), 64)
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println("Value of number:", f)
+    }
 }
