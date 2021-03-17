27a32dd97bb57dd82d14d65d9f1a787501b92540
Weigang Geng <gengwg@fb.com>
2021-03-17 10:59:44 -0700

    time.Now()

--- Exercise Files/Ch03/03_04/datesAndTimes.go
+++ Exercise Files/Ch03/03_04/datesAndTimes.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
 	fmt.Printf("Go launched at %s\n", t)
+
+	now := time.Now()
+	fmt.Printf("The time now is %s\n", now)
 }
