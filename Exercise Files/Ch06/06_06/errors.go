a75ed80e722a7a4c68c791ef7b0ce95b5e401561
Weigang Geng <gengwg@fb.com>
2021-03-29 23:22:28 -0700

    handling errors

--- Exercise Files/Ch06/06_06/errors.go
+++ Exercise Files/Ch06/06_06/errors.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"errors"
 )
 
 func main() {
 	f, err := os.Open("filename.ext")
 
-	// attendance := map[string]bool{
-	// 	"Ann": true,
-	// 	"Mike": true}
+	if err == nil {
+		fmt.Println(f)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	myError := errors.New("My error string")
+	fmt.Println(myError)
+
+	attendance := map[string]bool{
+		"Ann": true,
+		"Mike": true}
+
+	attended, ok := attendance["Mike"]
+	if ok {
+		fmt.Println("Mike attended?", attended)
+	} else {
+		fmt.Println("No info for Mike")
+	}
 }
