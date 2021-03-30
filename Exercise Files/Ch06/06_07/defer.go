b5e17f36f047d45e900ae8873dc559910f11fa69
Weigang Geng <gengwg@fb.com>
2021-03-29 23:38:08 -0700

    deferring function calls

--- Exercise Files/Ch06/06_07/defer.go
+++ Exercise Files/Ch06/06_07/defer.go
@@ -6,8 +6,32 @@ import (
 
 func main() {
 
+	defer fmt.Println("Close the file!")
+	fmt.Println("Open the file!")
+
+	// stack; lifo
+	defer fmt.Println("Statement 1")
+	defer fmt.Println("Statement 2")
+
+	myFunc()
+
+	defer fmt.Println("Statement 3")
+	defer fmt.Println("Statement 4")
+	fmt.Println("Undeferred Statement")
+
+	x := 1000
+	// evaluated and saved at the moment of deferring
+	defer fmt.Println("Value of x:", x)
+	x++
+	fmt.Println("Value of x after incrementing: ", x)
+
+
 }
 
+// defer only works within a function
 func myFunc() {
 
+	defer fmt.Println("Deferred in the function")
+
+	fmt.Println("Not Deferred in the function")
 }
