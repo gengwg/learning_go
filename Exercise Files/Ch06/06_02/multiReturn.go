7af9bb28a405f9631b3be125484d715ac783cfcb
Weigang Geng <gengwg@fb.com>
2021-03-29 22:32:42 -0700

    functions

--- Exercise Files/Ch06/06_02/multiReturn.go
+++ Exercise Files/Ch06/06_02/multiReturn.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	n1, l1 := FullName("Zaphod", "Beeblebox")
+	fmt.Printf("Fll name: %v, number of chars: %v", n1, l1)
+	FullName()
 
+}
+
+func FullName(f, l string) (string, int) {
+	full := f + " " + l
+	length := len(full)
+	return full, length
 }
