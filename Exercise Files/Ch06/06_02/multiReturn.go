98116b4e4c5d71da4b3cec1f155988739fcd17bb
Weigang Geng <gengwg@fb.com>
2021-03-29 22:38:01 -0700

    naked return

--- Exercise Files/Ch06/06_02/multiReturn.go
+++ Exercise Files/Ch06/06_02/multiReturn.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
+
 	n1, l1 := FullName("Zaphod", "Beeblebox")
-	fmt.Printf("Fll name: %v, number of chars: %v", n1, l1)
+	fmt.Printf("Fll name: %v, number of chars: %v\n", n1, l1)
+
+	n2, l2 := FullNameNakedReturn("Arther", "Glurberg")
+	fmt.Printf("Fll name: %v, number of chars: %v\n", n2, l2)
+
 }
 
 func FullName(f, l string) (string, int) {
 	full := f + " " + l
 	length := len(full)
 	return full, length
+}
+
+// same effect as above; define return value in function signature
+func FullNameNakedReturn(f, l string) (full string, length int) {
+	full = f + " " + l
+	length = len(full)
+	return
 }
