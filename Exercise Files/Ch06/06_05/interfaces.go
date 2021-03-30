ed3fa7724725c0f170d29211e37b5c8eaced65eb
Weigang Geng <gengwg@fb.com>
2021-03-29 23:11:10 -0700

    interfaces

--- Exercise Files/Ch06/06_05/interfaces.go
+++ Exercise Files/Ch06/06_05/interfaces.go
@@ -2,10 +2,37 @@ package main
 
 import "fmt"
 
+type Animal interface {
+	Speak() string
+}
+
 type Dog struct {
 }
 
+func (d Dog) Speak() string {
+	return "Woof!"
+}
+
+type Cat struct {
+}
+
+func (d Cat) Speak() string {
+	return "Meow!"
+}
+
+type Cow struct {
+}
+
+func (d Cow) Speak() string {
+	return "Moo!"
+}
+
 func main() {
-	poodle := Dog{}
+	poodle := Animal(Dog{})
 	fmt.Println(poodle)
+
+	animals := []Animal{Dog{}, Cat{}, Cow{}}
+	for _, animal := range animals {
+		fmt.Println(animal.Speak())
+	}
 }
