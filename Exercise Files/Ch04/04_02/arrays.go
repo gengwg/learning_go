41ccb1cdc5605f475fc833281eb6c92afccbf0c4
Weigang Geng <gengwg@fb.com>
2021-03-30 19:34:21 -0700

    arrays

--- Exercise Files/Ch04/04_02/arrays.go
+++ Exercise Files/Ch04/04_02/arrays.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	
+	// better use slices than array
+	var colors [3]string	// must be same type in array
+	colors[0] = "Red"		// type already defined
+	colors[1] = "Green"
+	colors[2] = "Blue"		// type already defined
+	fmt.Println(colors)
+	fmt.Println(colors[1])
+
+
+	// assingn values as you define array
+	var numbers = [5]int{5, 3, 1, 2, 4}
+	fmt.Println(numbers)
+
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers:", len(numbers))
+
 }
