a6e69cd25593847f4ffe4ba88d4904b8151c0c20
Weigang Geng <gengwg@fb.com>
2021-03-30 19:45:23 -0700

    slices

--- Exercise Files/Ch04/04_03/slices.go
+++ Exercise Files/Ch04/04_03/slices.go
@@ -2,8 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	
+
+	// without numbers in [] it's slice; with number its array
+	var colors = []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
+
+	// slices are resizable
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	// remove 1st item
+	colors = append(colors[1:len(colors)])
+	fmt.Println(colors)
+
+	// remove last item
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	// type, initial size, capcity
+	numbers := make([]int, 5, 5)
+	numbers[0] = 1
+	numbers[1] = 72
+	numbers[2] = 32
+	numbers[3] = 12
+	numbers[4] = 156
+	fmt.Println(numbers)
+
+	numbers = append(numbers, 235)
+	fmt.Println(numbers)
+	fmt.Println(cap(numbers))	// output 10!
+
+	sort.Ints(numbers)
+	fmt.Println(numbers)
 }
