4bd5e5d3f26f2e99987ce7dcfded96d015cb6034
Weigang Geng <gengwg@fb.com>
2021-03-30 19:28:15 -0700

    referencing values with pointers

--- Exercise Files/Ch04/04_01/pointers.go
+++ Exercise Files/Ch04/04_01/pointers.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	
+
+	var p *int
+
+	if p != nil {
+		fmt.Println("value of p: ", *p)		// * references a pointer
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var v int = 42
+	p = &v	// & connects a variable and pointer
+	if p != nil {
+		fmt.Println("value of p: ", *p)
+	} else {
+		fmt.Println("p is nil")
+	}
+
+	var value1 float64 = 42.13
+	pointer1 := &value1
+	fmt.Println("Value 1:", *pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Value 1:", *pointer1)
+	fmt.Println("Value 1:", value1)
 }
