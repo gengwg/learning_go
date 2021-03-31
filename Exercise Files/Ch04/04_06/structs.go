e32249371b289b4fc8fa37d5aee14d0c1cf5a30a
Weigang Geng <gengwg@fb.com>
2021-03-30 23:21:50 -0700

    group related values in struct

--- Exercise Files/Ch04/04_06/structs.go
+++ Exercise Files/Ch04/04_06/structs.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+type Dog struct {
+	Breed string
+	Weight int
+}
+
 func main() {
+
+	poodle := Dog{"Poodle", 34}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)	// print details of struct
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight) // access fields of struct
+
 
 }
