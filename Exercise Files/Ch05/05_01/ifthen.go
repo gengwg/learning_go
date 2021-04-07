a75b402a82e972ad6b8cc89cde56d7357f92d324
Weigang Geng <gengwg@fb.com>
2021-04-07 15:40:02 -0700

    variable local to conditonal block

--- Exercise Files/Ch05/05_01/ifthen.go
+++ Exercise Files/Ch05/05_01/ifthen.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 
-	var x float64 = 42
+	// var x float64 = 42
 	var result string
 
-	if x < 0 {
+	// initial statement w/in conditional block
+	// value local to the conditional block
+	// memery released exiting block
+	if x:= 42; x < 0 {
 		result = "Less than zero"
 	} else if x == 0 {
 		result = "Equal to zero"
