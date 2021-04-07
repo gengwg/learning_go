2aae91b10e8137330d0512a35870d2979561eecc
Weigang Geng <gengwg@fb.com>
2021-04-07 15:56:22 -0700

    switch statement

--- Exercise Files/Ch05/05_02/switch.go
+++ Exercise Files/Ch05/05_02/switch.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"	
-) 
+	"time"
+)
 
 func main() {
 
 	rand.Seed(time.Now().Unix())
 	dow := rand.Intn(6) + 1
 	fmt.Println("Day", dow)
+
+	result := ""
+
+	switch dow {
+	case 1:
+		result = "It's Sunday!"
+	case 7:
+		result = "It's Saturday!"
+	default:
+		result = "It's a weekday!"
+	}
+
+	fmt.Println("Day", dow, ",", result)
 
 }
