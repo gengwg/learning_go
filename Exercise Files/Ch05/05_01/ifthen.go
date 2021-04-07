804cd0d07e93c1d9572c90d8e66b547862bcd0c6
Weigang Geng <gengwg@fb.com>
2021-04-07 15:36:49 -0700

    conditional

--- Exercise Files/Ch05/05_01/ifthen.go
+++ Exercise Files/Ch05/05_01/ifthen.go
@@ -6,5 +6,14 @@ func main() {
 
 	var x float64 = 42
 	var result string
-	
+
+	if x < 0 {
+		result = "Less than zero"
+	} else if x == 0 {
+		result = "Equal to zero"
+	} else { // else must be on same line as closing bracket
+		result = "Greater than zero"
+	}
+
+	fmt.Println("Result:", result)
 }
