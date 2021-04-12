454d023718982adf427e4f36ebdf7bfaf0d9334a
Weigang Geng <gengwg@fb.com>
2021-04-11 22:01:55 -0700

    parsing json

--- Exercise Files/Ch07/07_05/parseJSON.go
+++ Exercise Files/Ch07/07_05/parseJSON.go
@@ -4,18 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	// "encoding/json"
-	// "strings"
-	// "math/big"
+	"encoding/json"
+	"strings"
+	"math/big"
 )
+
+// you need use the same name as json field
+type Tour struct {
+	Name, Price string	// Upper case exports the fields
+}
 
 func main() {
 
 	url := "http://services.explorecalifornia.org/json/tours.php"
 	content := contentFromServer(url)
 
-	fmt.Println(content)
+	tours := toursFromJson(content)
+	// fmt.Println(tours)
 
+	for _, tour := range tours {
+		price, _, _ := big.ParseFloat(tour.Price, 10, 2, big.ToZero)
+		fmt.Printf("%v ($%.2f)\n", tour.Name, price)
+	}
 }
 
 func checkError(err error) {
@@ -25,10 +35,10 @@ func checkError(err error) {
 }
 
 func contentFromServer(url string) string {
-	
+
 	resp, err := http.Get(url)
 	checkError(err)
-	
+
 	defer resp.Body.Close()
 	bytes, err := ioutil.ReadAll(resp.Body)
 	checkError(err)
@@ -36,3 +46,19 @@ func contentFromServer(url string) string {
 	return string(bytes)
 }
 
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+	}
+
+	return tours
+}
